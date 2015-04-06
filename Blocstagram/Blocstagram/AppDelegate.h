a61a83bbb7ac01582570a4d04cdb4bead0485667
Tim Pryor <tdpryor@gmail.com>
2015-04-06 15:29:54 -0400

    Initial Blocstagram commit

---
+++ Blocstagram/Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Tim on 2015-04-06.
+//  Copyright (c) 2015 Tim Pryor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blocstagram/Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Tim on 2015-04-06.
+//  Copyright (c) 2015 Tim Pryor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
